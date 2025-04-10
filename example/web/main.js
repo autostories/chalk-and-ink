a6f9490af646f6e7cb6cb8b6dc9d22dbbdf4ec4e
James Aprosail <aprosail@outlook.com>
2025-04-10 09:32:24 +0800

    feat: basic web icons
    1. feat: html, css, js, ts use plaint alphabet icon.
    2. feat: star icon for svg and favicon.
    3. chore: prepare web example workspace and apply monorepo.
    4. chore: inner prettier options to allow semi in js and ts.
    5. chore: disable file nesting in example folder.

---
+++ example/web/main.js
@@ -0,0 +1,8 @@
+function jsEntry() {
+  const message = "message";
+  // prettier-ignore
+  console.log('single quoted string');
+  console.log("double quoted string");
+  console.log(`template string: ${message}`);
+}
+jsEntry();
